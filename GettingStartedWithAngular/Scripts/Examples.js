9392f247aed79fb43869fc45b844fedd1f1e4968
Gajare <Sanjunath.Gajare@software-innovation.com>
2016-08-21 09:18:20 +0530

    2nd example committed

---
+++ GettingStartedWithAngular/Scripts/Examples.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="C:\D\Personal\Projects\GettingStartedWithAngular\GettingStartedWithAngular\Library/angular.js" />
+
+var exampleApp = angular.module("MyExampleModule", [])
+                 .controller("Example1Controller", function ($scope) {
+                     $scope.Example1 = "Welcome to getting started with angular"
+                     var employee = { name: "San", skils: ".Net", exp: 5 }
+                     $scope.employee = employee;
+
+                     var employees= [{ name: "sanju", dob: "04/09/1987", skils: "C#,Java,Angular", Exp: 5 },
+                                    { name: "Arush", dob: "01/09/1988", skils: "C#,Java,BIF", Exp: 3 },
+                                    { name: "Aneesh", dob: "01/09/1984", skils: "C#,Java,Sharepoint,BIF", Exp: 3 }];
+                     $scope.employees = employees;
+                 });
